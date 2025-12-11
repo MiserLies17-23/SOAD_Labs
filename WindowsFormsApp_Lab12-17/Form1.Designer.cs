2d342108ec6ad71e99f1cc454d3c58ff4be20fd6
MiesLies17-23 <vadimtolstouhov662@gmail.com>
2025-12-11 08:32:45 +0300

    init commit

---
+++ WindowsFormsApp_Lab12-17/Form1.Designer.cs
@@ -0,0 +1,188 @@
+﻿namespace WindowsFormsApp_Lab12_17
+{
+    partial class SortingForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewCheckBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.CompareButton = new System.Windows.Forms.Button();
+            this.ArraySizeNumeric = new System.Windows.Forms.NumericUpDown();
+            this.ArraySizeLabel = new System.Windows.Forms.Label();
+            this.ExitButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ArraySizeNumeric)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3,
+            this.Column4,
+            this.Column5});
+            this.dataGridView1.GridColor = System.Drawing.SystemColors.ActiveCaption;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 0);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(798, 230);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // Column1
+            // 
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle1.BackColor = System.Drawing.Color.Cyan;
+            dataGridViewCellStyle1.NullValue = false;
+            this.Column1.DefaultCellStyle = dataGridViewCellStyle1;
+            this.Column1.HeaderText = "";
+            this.Column1.MinimumWidth = 6;
+            this.Column1.Name = "Column1";
+            this.Column1.Resizable = System.Windows.Forms.DataGridViewTriState.True;
+            this.Column1.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.Automatic;
+            // 
+            // Column2
+            // 
+            dataGridViewCellStyle2.BackColor = System.Drawing.Color.Cyan;
+            this.Column2.DefaultCellStyle = dataGridViewCellStyle2;
+            this.Column2.HeaderText = "";
+            this.Column2.MinimumWidth = 6;
+            this.Column2.Name = "Column2";
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "Сравнения";
+            this.Column3.MinimumWidth = 6;
+            this.Column3.Name = "Column3";
+            // 
+            // Column4
+            // 
+            this.Column4.HeaderText = "Перестановки";
+            this.Column4.MinimumWidth = 6;
+            this.Column4.Name = "Column4";
+            // 
+            // Column5
+            // 
+            this.Column5.HeaderText = "Время";
+            this.Column5.MinimumWidth = 6;
+            this.Column5.Name = "Column5";
+            // 
+            // CompareButton
+            // 
+            this.CompareButton.Location = new System.Drawing.Point(12, 278);
+            this.CompareButton.Name = "CompareButton";
+            this.CompareButton.Size = new System.Drawing.Size(88, 23);
+            this.CompareButton.TabIndex = 1;
+            this.CompareButton.Text = "Сравнить";
+            this.CompareButton.UseVisualStyleBackColor = true;
+            // 
+            // ArraySizeNumeric
+            // 
+            this.ArraySizeNumeric.Location = new System.Drawing.Point(330, 279);
+            this.ArraySizeNumeric.Maximum = new decimal(new int[] {
+            1000000,
+            0,
+            0,
+            0});
+            this.ArraySizeNumeric.Name = "ArraySizeNumeric";
+            this.ArraySizeNumeric.Size = new System.Drawing.Size(120, 22);
+            this.ArraySizeNumeric.TabIndex = 2;
+            this.ArraySizeNumeric.Value = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+            // 
+            // ArraySizeLabel
+            // 
+            this.ArraySizeLabel.AutoSize = true;
+            this.ArraySizeLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.ArraySizeLabel.Location = new System.Drawing.Point(175, 281);
+            this.ArraySizeLabel.Name = "ArraySizeLabel";
+            this.ArraySizeLabel.Size = new System.Drawing.Size(129, 16);
+            this.ArraySizeLabel.TabIndex = 3;
+            this.ArraySizeLabel.Text = "Размер массива";
+            // 
+            // ExitButton
+            // 
+            this.ExitButton.BackgroundImage = global::WindowsFormsApp_Lab12_17.Properties.Resources.button_exit_icon;
+            this.ExitButton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ExitButton.Location = new System.Drawing.Point(585, 257);
+            this.ExitButton.Name = "ExitButton";
+            this.ExitButton.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.ExitButton.Size = new System.Drawing.Size(92, 65);
+            this.ExitButton.TabIndex = 4;
+            this.ExitButton.UseVisualStyleBackColor = true;
+            this.ExitButton.Click += new System.EventHandler(this.ExitButton_Click);
+            // 
+            // SortingForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(800, 363);
+            this.Controls.Add(this.ExitButton);
+            this.Controls.Add(this.ArraySizeLabel);
+            this.Controls.Add(this.ArraySizeNumeric);
+            this.Controls.Add(this.CompareButton);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "SortingForm";
+            this.Text = "24ВП1_Толстоухов_Лаб12-17";
+            this.Load += new System.EventHandler(this.SortingForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ArraySizeNumeric)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button CompareButton;
+        private System.Windows.Forms.NumericUpDown ArraySizeNumeric;
+        private System.Windows.Forms.Label ArraySizeLabel;
+        private System.Windows.Forms.Button ExitButton;
+        private System.Windows.Forms.DataGridViewCheckBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column5;
+    }
+}
+
---
+++ WindowsFormsApp_Lab12-17/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp_Lab12_17
+{
+    public partial class SortingForm : Form
+    {
+        public SortingForm()
+        {
+            InitializeComponent();
+        }
+
+        private void SortingForm_Load(object sender, EventArgs e)
+        {
+            DataGridView_Initilize();
+        }
+
+        private void DataGridView_Initilize()
+        {
+            dataGridView1.RowCount = 7;
+            dataGridView1.Rows[0].Cells[1].Value = "Обмен";
+            dataGridView1.Rows[1].Cells[1].Value = "Выбор";
+            dataGridView1.Rows[2].Cells[1].Value = "Включение";
+            dataGridView1.Rows[3].Cells[1].Value = "Шелла";
+            dataGridView1.Rows[4].Cells[1].Value = "Быстрая";
+            dataGridView1.Rows[5].Cells[1].Value = "Линейная";
+            dataGridView1.Rows[6].Cells[1].Value = "Встроенная";
+        }
+
+        private void ExitButton_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+    }
+}
